e0cc9bec82b9424d51c0285908a804aafe2df053
Joshua Blew <joshua@normalfinance.io>
2023-08-02 12:54:23 -0500

    small project fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -200,6 +200,7 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+    excludeContracts: ["test/"],
   },
   etherscan: {
     apiKey: {
